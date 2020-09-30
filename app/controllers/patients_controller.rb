78ce47f780ee1a9d4b7400e023224b31e3ffbed0
Austin Cummings <aac729@live.com>
2020-09-30 22:17:18 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,14 @@
 class PatientsController < ApplicationController
+
   def index
     @patients = Patient.all
-    @patients 
   end
 
   def show
     @patient = Patient.find(params[:id])
   end
+
+
+
+
 end
